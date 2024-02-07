4132d3cef241fd225aa00e2b0496838c357a39f2
avsaryagmur <avsaryagmurr@gmail.com>
2024-02-07 22:29:11 +0300

    for Future tests

--- cypress/e2e/1-getting-started/moreandmore.cy.js
+++ cypress/e2e/1-getting-started/moreandmore.cy.js
@@ -31,7 +31,31 @@ describe("more yagmur - will update", ()=> {
     })
   
   
-  
+    it.skip('Should Get the Created Gist Successfully' , ()=> {
+      cy.readFile('cypress/fixtures/gistAPIResponse.json').then(({ createdGistId , createdGistURL, publicFileNameText, descriptionAPIText}) => {
+
+            const request = {
+              method: "PATCH",
+              url: `https://api.github.com/gists/${createdGistId}`,
+              headers: {
+                authorization,
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+              },
+              body: {
+                description:"An updated gist description",
+                files:{"README.md":
+                {content:"Hello World from GitHub"}
+              }}
+              
+            }
+
+          cy.request(request).then((response) => {
+            cy.log(response.status);
+
+          })
+        });
+    })
   })
   
 
